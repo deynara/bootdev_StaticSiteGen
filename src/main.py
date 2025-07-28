deda5a15f54ca07479cae98bdecca0403b8c714d
deynara <REDACTED>
2025-07-28 07:25:04 +0200

    implemented markdown block to block_type

--- src/main.py
+++ src/main.py
@@ -1,24 +1,45 @@
 from textnode import TextNode, TextType, textnode_to_htmlnode
 from htmlnode import *
 from markdown import *
+from markdown_blocks import *
 from debughelper import *
 
 def main():
     print("Starting main.py")
     md = """
-This is **bolded** paragraph
+# This is a heading
 
-This is another paragraph with _italic_ text and `code` here
-This is the same paragraph on a new line
+This is a paragraph of text. It has some **bold** and _italic_ words inside of it.
 
+- This is the first list item in a list block
+- This is a list item
+- This is another list item
 
+> This is a quote.
+> This is also a quote.
 
+```
+This is code
+```
 
-- This is a list
-- with items
+> This is a single quote.
+
+> That's a different quote.
+But this is no quote
+
+# Heading 1
+
+1. Item 1
+2. Item 2
+3. Item 3
+
+## Heading 2
 """
     blocks = markdown_to_blocks(md)
-    debug_pretty_print_list(blocks, "Blocks")
+    #debug_pretty_print_list(blocks, "Blocks")
+    for block in blocks:
+        typ = block_to_block_type(block)
+        print (block, "\n", typ, "\n")
     #print(md)
 if __name__ == "__main__":
     main()
--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,9 +1,78 @@
+from enum import Enum
+import re
+from debughelper import debug_pretty_print_list
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE ="code"
+    QUOTE = "quote"
+    ULIST = "unordered_list"
+    OLIST = "ordered_list"
+    MULTI = "multi" # 
+
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
-    #debug_pretty_print_list(blocks)
     blocks = [block.strip() for block in blocks]
     while "" in blocks:
         blocks.remove("")
-    
-    #debug_pretty_print_list(blocks)
     return blocks
+
+def block_to_block_type(block):
+    pattern = r"#{1,6} "
+    #print("Matchtry Heading", re.match(pattern, block))
+    if re.match(pattern, block):
+        return BlockType.HEADING
+    pattern = r"`{3}"
+
+    if re.match(pattern, block) and re.match(pattern, block[:-2]):
+        if len(block) < len("123456"):
+            #accidental match, block shorter than a code block can be. Example case block is |'''|
+            return BlockType.PARAGRAPH
+        return BlockType.CODE
+
+    line_list = [block]
+    if "\n" in block:
+        line_list = block.split("\n")
+
+    pattern = r">"
+    if re.match(pattern, line_list[0]):
+        if len(line_list) == 1:
+            return BlockType.QUOTE
+        return multiline_block(pattern, line_list, BlockType.QUOTE)
+
+    pattern = r"- "
+    if re.match(pattern, line_list[0]):
+        if len(line_list) == 1:
+            return BlockType.ULIST
+        return multiline_block(pattern, line_list, BlockType.ULIST)
+    
+    pattern = r"\d\. "
+    if re.match(pattern, line_list[0]):
+        if len(line_list) == 1:
+            return BlockType.OLIST
+        return multiline_block(pattern, line_list, BlockType.OLIST, True)
+
+    return BlockType.PARAGRAPH
+
+def multiline_block(pattern, line_list, typ, check_order_sanity = False):
+    if re.match(pattern, line_list[0]):
+        all_match = True
+        for i in range(0, len(line_list)):
+            if re.match(pattern, line_list[i]):
+                if check_order_sanity:
+                    if str(i+ 1) != line_list[i][0]:
+                        all_match = False
+                        break
+                continue
+            else:
+                all_match = False
+                return BlockType.MULTI
+        if all_match:
+            if check_order_sanity:
+                pass
+            return typ
+    return
+
